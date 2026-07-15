42ff1c38710ea29bf2fd40ec66ef8e2bf7f78ac6
vlad3467 <vlad.poverin@gmail.com>
2026-07-15 14:41:05 +0300

    Статическая сборка

--- build/Desktop_Qt_6_11_1_static_MinGW_w64_MINGW64_MSYS2_Debug/qtcsettings.cmake
+++ build/Desktop_Qt_6_11_1_static_MinGW_w64_MINGW64_MSYS2_Debug/qtcsettings.cmake
@@ -1,14 +1,2 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
-set("CMAKE_CXX_COMPILER" "C:/msys64/mingw64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_C_COMPILER" "C:/msys64/mingw64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_MAKE_PROGRAM" "C:/msys64/mingw64/bin/ninja.exe" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "C:/msys64/mingw64/qt6-static" CACHE "PATH" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/REPOS/PortSniffer/build/Desktop_Qt_6_11_1_static_MinGW_w64_MINGW64_MSYS2_Debug/.qtcreator/cmake-helper/qtcreator-project.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_CREATOR_ENABLE_MAINTENANCE_TOOL_PROVIDER" "ON" CACHE "BOOL" "" FORCE)
-set("QT_CREATOR_ENABLE_PACKAGE_MANAGER_SETUP" "ON" CACHE "BOOL" "" FORCE)
-set("QT_ENABLE_QML_DEBUG" "ON" CACHE "BOOL" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "C:/msys64/mingw64/qt6-static/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
